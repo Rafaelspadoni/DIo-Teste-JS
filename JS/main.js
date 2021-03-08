21a8b860f356c9e32cad1844a9c786e425c71e67
Rafaelspadoni <rafaspadonisolid16@hotmail.com>
2021-03-07 22:09:10 -0300

    arrays

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,15 @@
-var nome = "The Ridller";
+
+
+/*var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "));
+*/
+
+/*var nome = "The Ridller";
 var idade = 18;
-alert("bem vindo " + nome + " sua idade é " + idade);
+//alert("bem vindo " + nome + " sua idade é " + idade);
+console.log(nome.replace("rafael"));
+*/
